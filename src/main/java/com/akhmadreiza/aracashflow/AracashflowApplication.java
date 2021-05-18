7e6779aca26050490afd9ee459421cb31ef99c09
Akhmad Reiza <reizaarmando@gmail.com>
2021-05-18 15:43:41 +0700

    initial commit

---
+++ src/main/java/com/akhmadreiza/aracashflow/AracashflowApplication.java
@@ -0,0 +1,13 @@
+package com.akhmadreiza.aracashflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AracashflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AracashflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhmadreiza/aracashflow/AracashflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhmadreiza.aracashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AracashflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
